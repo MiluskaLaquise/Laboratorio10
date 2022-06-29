9e6495c105589b5ccdc4463f3b89951f43af969e
MiluskaLaquise <miluska.laquise@tecsup.edu.pe>
2022-06-28 23:19:51 -0500

    first commit

---
+++ app/Models/comprobante.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class comprobante extends Model
+{
+    use HasFactory;
+    protected $table ='comprobante';
+}
